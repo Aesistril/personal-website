b75c12db65d3c5cc50d73ff8e778711368ce9dff
Yigit Ayaz <yigitayaz262@gmail.com>
2022-01-15 20:07:14 +0300

    made a script for  importing external html

---
+++ exthtml/makejs.sh
@@ -0,0 +1,9 @@
+for filename in ./html/*; do
+  NAME="$(basename $filename .html)"
+  JSNAME="${NAME}.js"
+  ESCAPED="$(sed 's/\\/\\\\/g;s/^.*$/&\\/g;s/'\''/\\'\''/g' $filename)"
+  cat <<EOF > ./js/$JSNAME
+  document.write('$ESCAPED
+  ')
+EOF
+done
